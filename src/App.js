ec90a1c6296a9f999baa7f92db24e27e8da5f32a
Jay dee <47309416+JDEE30@users.noreply.github.com>
2021-04-07 10:08:52 -0500

    update app,js

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import 'materialize-css/dist/css/materialize.min.css';
+
+function App() {
+  return (
+    <div>
+ <nav>
+      <div>
+         <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>  
+         </ul>
+      </div>
+    </nav>    
+</div>
+
+  
+  );
+
+}
+
+export default App;
+

03f67bb1d84e0721fd450057b7bbf969b7bc0a39
Jay dee <47309416+JDEE30@users.noreply.github.com>
2021-05-05 21:43:17 -0500

    update

--- src/map.js
+++ src/map.js
@@ -6,7 +6,7 @@ class Map extends Component {
     render() {
         return (
             <div id="Map">
-<iframe class="mapframe" loading="lazy" allowfullscreen src="https://www.google.com/maps/embed/v1/place?q=San%20An&key=AIzaSyBTqw_XIWmlSOfRfEWfvWGod2OSzLkIQDA&zoom=12"></iframe>
+<iframe class="mapframe" loading="lazy" allowfullscreen src="https://www.google.com/maps/embed/v1/place?q=San%20An&key=&zoom=12"></iframe>
 </div>
       );                                                                             
 }
